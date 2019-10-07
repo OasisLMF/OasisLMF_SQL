adf689c969b0709a2da82b0afd6a093453ffb97f
benhayes21 <Ben.hayes@oasislmf.org>
2019-10-07 17:52:13 +0100

    remove typo

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except ImportError:
     from urllib2 import urlopen, URLError
 
 
-KTOOLS_VERSION = '3.1.3https://github.com/OasisLMF/OasisLMF_SQL'
+KTOOLS_VERSION = '3.1.3evaluation'
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
 
